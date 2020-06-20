6fa67a87f5d23af0a27f1439bc02d9b761e41cba
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-20 11:58:24 -0500

    logout button works

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,11 +1,20 @@
-import '../styles/main.scss';
+import firebase from 'firebase/app';
+import apiKeys from './helpers/apiKeys.json';
+
+import auth from './components/auth/auth';
+import myNavbar from './components/myNavbar/myNavbar';
+import authData from './helpers/data/authData';
+
 import 'bootstrap';
-import auth from './components/auth/auth'
+import '../styles/main.scss';
 
 console.error('Mushrooms!');
 
 const init = () => {
+  firebase.initializeApp(apiKeys.firebaseConfig);
+  authData.checkLoginStatus();
   auth.loginButton();
-}
+  myNavbar.logoutEvent();
+};
 
 init();
