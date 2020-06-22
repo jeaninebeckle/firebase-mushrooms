05ebf94bb95542fe864661eed5cd1c0b33e41fd7
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-06-22 18:54:25 -0500

    use promise to format mushrooms into an array of objects

--- src/javascripts/components/mushroomList/mushroomList.js
+++ src/javascripts/components/mushroomList/mushroomList.js
@@ -3,7 +3,7 @@ import mushroomData from '../../helpers/data/mushroomData';
 
 const buildForest = () => {
   mushroomData.getMushrooms()
-    .then((response) => console.error('Get mushrooms worked!', response.data))
+    .then((mushrooms) => console.error('Get mushrooms worked!', mushrooms))
     .catch((err) => console.error('it broke', err));
   // const domString = '<h1>I SEE MUSHROOMS!!!</h1>';
   // utils.printToDom('#forest', domString);
